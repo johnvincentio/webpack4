0453eec5ae5f3934e7f0d7044d4cb9adc1a357c4
johnvincentio <john@johnvincent.io>
2018-10-27 12:32:07 -0400

    add

---
+++ webpack-splitting-example/dist/1.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./src/my-module-1.js":
+/*!****************************!*\
+  !*** ./src/my-module-1.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return greetingOne; });\nfunction greetingOne() {\n  const el = document.createElement(\"div\")\n  \n  el.innerText = \"Hello from my module!\"\n\n  document.body.appendChild(el)\n}\n\n\n//# sourceURL=webpack:///./src/my-module-1.js?");
+
+/***/ })
+
+}]);
